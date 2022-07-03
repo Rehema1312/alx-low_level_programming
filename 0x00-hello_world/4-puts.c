22578c5eabf7375d272004861e88cdc7e0e02a88
Rehema1312 <rehemangoli84@gmail.com>
2022-07-03 06:37:19 -0700

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *
+ *  *main - using puts instead of printf
+ *
+ *   *
+ *
+ *    *Return 0
+ *
+ *    **/
+int main(void)
+{
+		char phrase[] = puts "\"Programming is like building a multilingual puzzle";
+		puts(phrase);
+		return (0);
+}
+
