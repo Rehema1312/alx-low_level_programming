25497dd24dc02492039710a7cff090ed443ea5c2
Rehema1312 <rehemangoli84@gmail.com>
2022-07-04 13:10:52 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main- ascending order
+ * return: always return 0 (success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x % 10 + '0');
+		if (x < 9)
+		{
+			putchar(', ');
+			putchar(', ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
