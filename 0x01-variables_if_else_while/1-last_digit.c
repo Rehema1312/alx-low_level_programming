d1b9dda8c1d7d998d58c92f0e82449c8d73255b6
Rehema1312 <rehemangoli84@gmail.com>
2022-07-04 09:52:32 -0700

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,11 +18,11 @@ int main(void)
 	{
 		printf("and is greater than 5");
 	}
-	if (x == 0)
+	if (last == 0)
 	{
 		printf("and is 0");
 	}
-	if (x < 6 && x != 0)
+	if (last < 6 && last != 0)
 	{
 		printf("and is less than 6 not 0");
 	}
