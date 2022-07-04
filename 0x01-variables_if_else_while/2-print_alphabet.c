a995d0fa9eb803cfd46b4ecb6b4a5b8a2c6808dd
Rehema1312 <rehemangoli84@gmail.com>
2022-07-04 11:15:11 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main-putchar function
+ * Return- always return0 (success)
+ */
+main(void)
+{
+		int ch;
+
+		for (ch = 'a'; ch < = 'z'; ch++)
+			putchar(ch);
+		putchar ('\n');
+		/*print lowercase letters*/
+		getch();
+}
