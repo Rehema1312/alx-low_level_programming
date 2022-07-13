181001e8f14d7580be9432c315ee852c6b980449
Rehema1312 <rehemangoli84@gmail.com>
2022-07-13 13:46:38 -0700

    concatenation

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <string.h>
 /**
+ * @dest: parameter pointer to a character
+ * @src: parameter pointer to a character
+ * Return: return resulting string dest
  * *_strcat - function that concatenates two strings
  */
 char *_strcat(char *dest, char *src);
@@ -9,3 +12,6 @@ char *_strcat(char *dest, char *src);
 	char *dest;
 	char *src;
 
+	strcat(*dest *src);
+	return (0);
+}
