e927807064a6f88aa3249c34d21cabbf9eafcf2c
Rehema1312 <rehemangoli84@gmail.com>
2022-07-19 11:39:09 -0700

    1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,10 +8,10 @@
  * @n: n amount of times
  * Return: return the string value
  */
-char *_memcpy(char *dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n);
 {
 		unsigned int i;
-		
+
 		for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
