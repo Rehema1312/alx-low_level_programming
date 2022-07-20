7d410399c8724b14c8eb5c6195c9b22a946b3531
Rehema1312 <rehemangoli84@gmail.com>
2022-07-20 06:59:11 -0700

    0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts: function that prints a string 
+ * @s: printing a character
+ * Return: always success
+ */ 
+void _puts_recursion(char *s)
+{
+	char x;
+
+	printf("%c/n", x);
+	return (0);
+}
