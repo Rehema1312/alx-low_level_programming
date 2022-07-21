99ab435526530cd18e677619b0ad9523797fde94
Rehema1312 <rehemangoli84@gmail.com>
2022-07-21 01:15:47 -0700

    0

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *  _sqrt_recursion - fution to return square root of numbers
+ *  @n: parameter to be tested
+ *  return: Always return SUCCESS
+ */
+int _sqrt_recursion(int n);
+{
+	if ()
+	{
+		return (n);
+	}
+	else
+	{
+		return (-1);
+	}
