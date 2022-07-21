ca7a2063fa1ffff20cb8406bd8448297b2e08d12
Rehema1312 <rehemangoli84@gmail.com>
2022-07-21 00:51:00 -0700

    function for prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * is_prime_number - function returning 1 if the integer is a prime number otherwise return 0
+ * is_prime_number - returns 1 on a prime integer otherwise return 0
  * @n: parameter to be tested
  * Return: always return Success
  */
 int is_prime_number(int n)
 {
-	if ( n / 2 )
+	if (n > 1 && n % 2 && n % 5 && n % 3 && n % 7 && n % 9)
 	{
 		return (1);
 	}
