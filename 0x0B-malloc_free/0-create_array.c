464b2b5e3c7d84750ebb35f7d50f6d4a6a5ebb3d
Rehema1312 <rehemangoli84@gmail.com>
2022-07-26 07:00:58 -0700

    First trial

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *create_array: function that creates array of chars intializes with a specific char
+ * @c: variable to be checked
+ * @size: variable to be checked
+ */
+char *create_array(unsigned int size, char c);
+{
+
