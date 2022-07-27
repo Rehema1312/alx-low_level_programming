af7c7be7a61f381625fb6c747f332fb846dbac20
Rehema1312 <rehemangoli84@gmail.com>
2022-07-27 05:18:12 -0700

    has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * @*str: char to contain copy of string
+ * *_strdup - returning pointer to allocated space
+ * Return: pointer to space in memory
+ */
+char *_strdup(char *str)
+{
+	int *x;
+	char y;
+	{
+		if(str == NULL)
+		{
+			return (NULL);
+		}
+	}
+	y = "str"
+	y = malloc (char y)
+		if( y = *x;)
+		{
+			return (*x)
+		}
+}
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * @s1: pointer to char
+ * @s2: pointer to char
+ * *str_concat - function that concatenates two strings
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int *x;
+
+	if( *x == NULL)
+	{
